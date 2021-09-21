184e9637d0c7ee5f3d8430de7e1f69e3059d67ae
Henrique <riquessan@gmail.com>
2021-09-21 09:33:09 -0300

    Component Movie Card

--- components/molecules/MovieCard/index.tsx
+++ components/molecules/MovieCard/index.tsx
@@ -1,14 +1,35 @@
-import React from "react";
-import { Subtitle } from "../../atoms/Subtitle";
-import { Text } from "../../atoms/Text";
-import { Title } from "../../atoms/Title";
+import React from 'react';
+import Image from 'next/image';
+import { MovieCardContainer, MovieDetailsContainer } from './styles';
+import { Title } from '../../atoms/Title';
+import { Text } from '../../atoms/Text';
+import { Subtitle } from '../../atoms/Subtitle';
 
-export const MovieCard = () => {
-  return (
-    <>
-      <Title>Movie</Title>
-      <Subtitle>Movie</Subtitle>
-      <Text>Movie</Text>
-    </>
-  );
-};
+interface IProps {
+  title: string;
+  date: string;
+  image: string;
+  overview: string;
+}
+
+export const MovieCard = ({
+  title,
+  date,
+  overview,
+  image,
+}: IProps): JSX.Element => (
+  <MovieCardContainer>
+    <Image
+      src={`https://www.themoviedb.org/t/p/w94_and_h141_bestv2/${image}`}
+      width={94}
+      height={141}
+    />
+    <MovieDetailsContainer>
+      <>
+        <Title>{title}</Title>
+        <Subtitle>{date}</Subtitle>
+      </>
+      <Text>{overview}</Text>
+    </MovieDetailsContainer>
+  </MovieCardContainer>
+);
---
+++ components/molecules/MovieCard/styles.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const MovieCardContainer = styled.div`
+  display: flex;
+
+  border-radius: ${props => props.theme.border.radius.md};
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 2px 8px 0px;
+
+  img {
+    border-radius: ${props =>
+      `${props.theme.border.radius.md} 0 0 ${props.theme.border.radius.md}`};
+  }
+`;
+
+export const MovieDetailsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: ${props => props.theme.spacing.margin.sm};
+  height: 121px;
+  width: fit-content;
+  padding: ${props =>
+    `${props.theme.spacing.padding.md} ${props.theme.spacing.padding.lg}`};
+
+  p {
+    display: -webkit-box;
+    overflow: hidden;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    text-overflow: ellipsis;
+  }
+`;
