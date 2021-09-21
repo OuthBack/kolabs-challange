6280ee3576e07d9d27cc39d3830d3fdff64bbcff
Henrique <riquessan@gmail.com>
2021-09-21 09:39:16 -0300

    Component Search

--- components/templates/Search/index.tsx
+++ components/templates/Search/index.tsx
@@ -1,6 +1,36 @@
-import React from "react";
-import { MovieList } from "../../organisms/MovieList";
+import React from 'react';
+import { SearchBar } from '../../atoms/SearchBar';
+import {
+  FilterMovieList,
+  IFilterMovieListProps,
+} from '../../organisms/FilterMovieList';
+import { MovieList } from '../../organisms/MovieList';
+import { IMovie, TPromiseQuery } from '../../types/useSeachMovies';
+import { SearchContainer } from './styles';
 
-export const Search = () => {
-  return <MovieList></MovieList>;
-};
+interface IProps extends IFilterMovieListProps {
+  filterNames: string[];
+  title: string;
+  tip: string;
+  movies: IMovie[];
+  error: Error | undefined;
+  searchMovies: TPromiseQuery;
+}
+
+export const Search = ({
+  filterNames,
+  title,
+  tip,
+  movies,
+  error,
+  searchMovies,
+}: IProps): JSX.Element => (
+  <>
+    <SearchBar onSearch={searchMovies} />
+    <SearchContainer>
+      <FilterMovieList tip={tip} title={title} filterNames={filterNames} />
+
+      <MovieList movies={movies} error={error} />
+    </SearchContainer>
+  </>
+);
---
+++ components/templates/Search/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const SearchContainer = styled.div`
+  display: flex;
+  gap: ${props => props.theme.spacing.margin.lg};
+  margin: 0 auto;
+  padding: ${props => props.theme.spacing.padding.lg};
+`;
