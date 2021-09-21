5b61bea83784cc52b6f873874623e5de0143464c
Henrique <riquessan@gmail.com>
2021-09-21 09:31:18 -0300

    Component Title

--- components/atoms/Title/index.tsx
+++ components/atoms/Title/index.tsx
@@ -1,7 +1,8 @@
-import React from "react";
-import { IChildren } from "../../types";
-import { H2 } from "./styles";
+import React from 'react';
+import { IFontColorProps } from '../../types/components';
+import { H2 } from './styles';
 
-export const Title = ({ children }: IChildren) => {
-  return <H2>{children}</H2>;
-};
+export const Title = ({
+  children,
+  color = 'black',
+}: IFontColorProps): JSX.Element => <H2 color={color}>{children}</H2>;
--- components/atoms/Title/styles.ts
+++ components/atoms/Title/styles.ts
@@ -1,3 +1,9 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import { chooseFontColor } from '../../../helpers/colors';
+import { IColor } from '../../types/components';
 
-export const H2 = styled.h2``;
+export const H2 = styled.h2<IColor>`
+  font-weight: 400;
+  font-size: 1.2em;
+  color: ${props => chooseFontColor(props.color, props.theme)};
+`;
