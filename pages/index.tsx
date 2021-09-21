ca0e5f261a0b01159a776e1acdcd04c200aa68df
Henrique <riquessan@gmail.com>
2021-09-21 09:44:40 -0300

    Page Changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,31 @@
-import type { NextPage } from "next";
-import { Search } from "../components/templates/Search";
-import styles from "../styles/Home.module.css";
+import React, { useEffect } from 'react';
+import type { NextPage } from 'next';
+import { Search } from '../components/templates/Search';
+import { useSearchMovies } from '../hooks/useSearchMovies';
 
-const Home: NextPage = () => {
-  return <Search></Search>;
+const filterNames = ['Tudo', 'Filmes', 'Seriados', 'Pessoas'];
+
+const title = 'Resultado da Busca';
+const tip =
+  "Dica: Você pode usar o filtro 'y:' para limitar seus resultados por ano. Exemplo: 'tropa de elite y:2007'.";
+
+const Home: NextPage = (): JSX.Element => {
+  const { movies, error, searchMovies, getPopular } = useSearchMovies();
+
+  useEffect(() => {
+    getPopular();
+  }, [getPopular]);
+
+  return (
+    <Search
+      tip={tip}
+      title={title}
+      filterNames={filterNames}
+      movies={movies}
+      error={error}
+      searchMovies={searchMovies}
+    />
+  );
 };
 
 export default Home;
