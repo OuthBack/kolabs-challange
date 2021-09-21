c3667c1a4ccf0e53cd3e3da1983eb08315fa47d6
Henrique <riquessan@gmail.com>
2021-09-21 09:35:00 -0300

    Component Movie List

--- components/organisms/MovieList/index.tsx
+++ components/organisms/MovieList/index.tsx
@@ -1,6 +1,33 @@
-import React from "react";
-import { MovieCard } from "../../molecules/MovieCard";
+import React from 'react';
+import { MovieCard } from '../../molecules/MovieCard';
+import { IMovie } from '../../types/useSeachMovies';
+import { MovieListContainer } from './styles';
 
-export const MovieList = () => {
-  return <MovieCard></MovieCard>;
+interface IProps {
+  movies: IMovie[];
+  error: Error | undefined;
+}
+
+const extendedDate = (movie_date: string): string => {
+  const date = new Date(movie_date);
+
+  return date.toLocaleDateString('pt-BR', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
 };
+
+export const MovieList = ({ movies, error }: IProps): JSX.Element => (
+  <MovieListContainer>
+    {movies.map(movie => (
+      <MovieCard
+        title={movie.title}
+        date={extendedDate(movie.release_date)}
+        overview={movie.overview}
+        image={movie.poster_path}
+        key={movie.id}
+      />
+    ))}
+  </MovieListContainer>
+);
---
+++ components/organisms/MovieList/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const MovieListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 22px;
+  width: 100%;
+`;
