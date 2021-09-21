f405d9d5f86a88179fe102db0d7ba7897e0887ee
Henrique <riquessan@gmail.com>
2021-09-21 09:48:29 -0300

    Search Service

---
+++ services/search/index.tsx
@@ -0,0 +1,39 @@
+import {
+  IMovieSearch,
+  IMovieSearchReturn,
+} from '../../components/types/useSearchMovies';
+import { api } from '../../config';
+
+const statusHandler = (result: { data: any; status: number }) => {
+  switch (result.status) {
+    case 200:
+      return { items: result.data };
+
+    default:
+      return { error: new Error('An error ocurred fechting data') };
+  }
+};
+
+export const search = async (
+  query: string,
+  page: number,
+): Promise<IMovieSearchReturn> => {
+  const result = await api.get<IMovieSearch>('/search/movie', {
+    params: {
+      query,
+      page,
+    },
+  });
+
+  return statusHandler(result);
+};
+
+export const popular = async (page: number): Promise<IMovieSearchReturn> => {
+  const result = await api.get<IMovieSearch>('/movie/popular', {
+    params: {
+      page,
+    },
+  });
+
+  return statusHandler(result);
+};
