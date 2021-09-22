0df0833dc5daa1ccce0847efb398bd84502ca7d8
Henrique <riquessan@gmail.com>
2021-09-22 20:57:36 -0300

    useSearch fixes and updates

--- hooks/useSearchMovies/index.tsx
+++ hooks/useSearchMovies/index.tsx
@@ -6,24 +6,25 @@ import React, {
   useContext,
   useState,
 } from 'react';
-import {
-  IMovie,
-  IMovieSearch,
-  TPromiseQuery,
-} from '../../components/types/useSearchMovies';
-import { popular, search } from '../../services/search';
+import { IMovie } from '../../components/types/useSearchMovies';
+import { popularMovies, queryMovies } from '../../services/search';
 
 export interface SearchMoviesProviderProps {
   children?: React.ReactNode;
 }
 
 interface SearchMoviesContextData {
-  movies: IMovie[];
+  movies: Set<IMovie>;
   page: number;
   error?: Error;
+  query: string;
+  // eslint-disable-next-line no-unused-vars
+  changeQuery: (queryValue: string) => void;
   setPage: Dispatch<SetStateAction<number>>;
-  searchMovies: TPromiseQuery;
+  searchMovies: () => void;
   getPopular: () => void;
+  loadPage(): void;
+  loading: boolean;
 }
 
 const SearchMoviesContext = createContext<SearchMoviesContextData>(
@@ -33,53 +34,94 @@ const SearchMoviesContext = createContext<SearchMoviesContextData>(
 export const SearchMoviesProvider = ({
   children,
 }: SearchMoviesProviderProps): JSX.Element => {
-  const [movies, setMovies] = useState<IMovie[]>([]);
+  const [query, setQuery] = useState<string>('');
+  const [movies, setMovies] = useState<Set<IMovie>>(new Set([]));
   const [page, setPage] = useState(1);
+  const [totalPages] = useState(500);
+  const [loading, setLoading] = useState(false);
+  const [canLoadPage, setCanLoadPage] = useState(true);
   const [error, setError] = useState<Error | undefined>();
 
-  const searchMovies = useCallback(
-    async query => {
-      const { items, error: SearchError } = await search(query, page);
-
-      // Para fazer o scroll infinito é preciso incrementar o array
-      // e para fazer a pesquisa é preciso resetar o array, por isso
-      // em toda página 1 ele reseta o array
-      const resetOrIncrementMovies = (arrayItems: IMovieSearch) => {
-        if (page === 1) {
-          setMovies(arrayItems.results);
-        } else {
-          setMovies(previousMovies =>
-            previousMovies.concat(arrayItems.results),
-          );
-        }
-      };
-
-      if (items) {
-        resetOrIncrementMovies(items);
+  const resetOrInscreaseMovies = useCallback(
+    (newMovies: IMovie[]) => {
+      if (page === 1) {
+        setMovies(new Set(newMovies));
         return;
       }
-      setError(SearchError);
+
+      setMovies(previousMovies => new Set([...previousMovies, ...newMovies]));
     },
     [page],
   );
 
+  const searchMovies = useCallback(async (): Promise<void> => {
+    if (!query) return;
+
+    const { items, error: SearchError } = await queryMovies(query, page);
+
+    setLoading(false);
+
+    if (error) {
+      setError(SearchError);
+      return;
+    }
+
+    if (!items) return;
+
+    if (items.results.length === 0) {
+      setMovies(new Set([]));
+      return;
+    }
+
+    // Para fazer o scroll infinito é preciso incrementar o array
+    // e para fazer a pesquisa é preciso resetar o array, por isso
+    // em toda página 1 ele reseta o array
+    resetOrInscreaseMovies(items.results);
+  }, [error, page, query, resetOrInscreaseMovies]);
+
   const getPopular = useCallback(async () => {
-    const { items, error: SearchError } = await popular(1);
+    if (!canLoadPage) return;
+    const { items, error: SearchError } = await popularMovies(page);
 
-    if (items) return setMovies(items.results);
+    setLoading(false);
 
-    return setError(SearchError);
-  }, []);
+    if (!items) return;
+    console.log(items.results.length);
+    resetOrInscreaseMovies(items.results);
+
+    setCanLoadPage(false);
+    setError(SearchError);
+  }, [canLoadPage, page, resetOrInscreaseMovies]);
+
+  const changeQuery = (queryValue: string) => {
+    setPage(1);
+    setQuery(queryValue);
+  };
+
+  const loadPage = () => {
+    if (page > totalPages) return;
+    setCanLoadPage(true);
+
+    setLoading(true);
+
+    console.log('loading', loading);
+
+    setPage(oldPage => oldPage + 1);
+  };
 
   return (
     <SearchMoviesContext.Provider
       value={{
         movies,
+        query,
+        changeQuery,
         page,
         setPage,
         error,
         searchMovies,
         getPopular,
+        loadPage,
+        loading,
       }}
     >
       {children}
