455110079d183096212dec5e1a6d1e9cc6fe6100
Henrique <riquessan@gmail.com>
2021-09-22 20:57:56 -0300

    Refactoring

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,33 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import type { NextPage } from 'next';
 import { Search } from '../components/templates/Search';
 import { useSearchMovies } from '../hooks/useSearchMovies';
 
-const filterNames = ['Tudo', 'Filmes', 'Seriados', 'Pessoas'];
+const filterNames = ['Filmes'];
 
 const title = 'Resultado da Busca';
 const tip =
   "Dica: Você pode usar o filtro 'y:' para limitar seus resultados por ano. Exemplo: 'tropa de elite y:2007'.";
 
 const Home: NextPage = (): JSX.Element => {
-  const { movies, error, searchMovies, getPopular } = useSearchMovies();
+  const {
+    movies,
+    error,
+    changeQuery,
+    loadPage,
+    searchMovies,
+    getPopular,
+    loading,
+  } = useSearchMovies();
+  const [filterSelected, setFilterSelected] = useState(0);
+
+  const changeFilter = (newFilter: number) => {
+    setFilterSelected(newFilter);
+  };
+
+  useEffect(() => {
+    searchMovies();
+  }, [searchMovies]);
 
   useEffect(() => {
     getPopular();
@@ -21,9 +38,13 @@ const Home: NextPage = (): JSX.Element => {
       tip={tip}
       title={title}
       filterNames={filterNames}
+      filterSelected={filterSelected}
+      changeFilter={changeFilter}
       movies={movies}
       error={error}
-      searchMovies={searchMovies}
+      changeQuery={changeQuery}
+      loadNewMovies={loadPage}
+      loading={loading}
     />
   );
 };
